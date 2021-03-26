871e18bb2d84a4d6a01c9ed7dd73538cbb6e290b
hi <hi@172.17.197.123>
2021-03-26 15:37:29 +0530

    its a controller

--- src/main/java/com/cg/creditcardbillpayment/controllers/CustomerControllerAdvice.java
+++ src/main/java/com/cg/creditcardbillpayment/controllers/CustomerControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.cg.creditcardbillpayment.exceptions.CustomerServiceException;
-
+//its a controller
 @RestControllerAdvice
 public class CustomerControllerAdvice {
 	@ExceptionHandler(CustomerServiceException.class)
